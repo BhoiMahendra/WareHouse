00bc2a75d13d40397d28794e2120673c3ae3e7e1
BhoiMahendra <34203668+BhoiMahendra@users.noreply.github.com>
2021-04-08 13:31:35 +0530

    Create WareHouse.java

---
+++ WareHouse.java
@@ -0,0 +1,23 @@
+package warehouse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WareHouse {
+	
+	RackController risk = new RackController();
+	public static void main (String args[]) {
+	
+		
+		RackController rack = new RackController();
+		
+		
+		rack.find("Article10");
+		
+		
+	}
+	
+
+}
